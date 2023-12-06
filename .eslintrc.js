1790c2e9b85931bb1ffd85c80038c6c36d71ef79
pengyabing <1754352510@qq.com>
2023-12-06 15:26:18 +0800

    feat: 增加husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,11 @@ module.exports = {
       },
     },
   ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
